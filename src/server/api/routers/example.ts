d4f51f4526c9555bdc1370211068952d3fa0dc42
ThyDrSlen <drslen9@gmail.com>
2023-07-11 09:17:18 -0400

    image changed

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -19,6 +19,6 @@ export const exampleRouter = createTRPCRouter({
   }),
 
   getSecretMessage: protectedProcedure.query(() => {
-    return "you can now see this secret message!";
+    return "you can now see this secret message! and you rock!";
   }),
 });
