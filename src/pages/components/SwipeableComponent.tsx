edf1bf2d67c9748d1bc9fa25cb422aca1f2b0db3
ThyDrSlen <drslen9@gmail.com>
2023-08-07 19:41:14 -0400

    fix with dynamic import

--- src/pages/components/SwipeableComponent.tsx
+++ src/pages/components/SwipeableComponent.tsx
@@ -1,42 +1,41 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useEffect,useRef } from "react";
+import dynamic from "next/dynamic";
 import ImageStack from "./ImageStack";
 
+const TinderCard = dynamic(() => import("react-tinder-card"), { ssr: false });
+
 function SwipeableComponent() {
+  const [imageIndex, setImageIndex] = useState(0);
   const [likes, setLikes] = useState(0);
   const [dislikes, setDislikes] = useState(0);
-  const [imageIndex, setImageIndex] = useState(0);
-  const [TinderCard, setTinderCard] = useState(null);
+  const [TinderCardLoaded, setTinderCardLoaded] = useState(false);
   const cardRef = useRef(null);
 
   useEffect(() => {
-    import('react-tinder-card').then((TinderCardModule) => {
-      setTinderCard(() => TinderCardModule.default);
-    });
+    setTinderCardLoaded(true);
   }, []);
 
+  const handleNextImage = () => {
+    setImageIndex((prevIndex) => prevIndex + 1);
+  };
+
   const onSwipe = (direction: string) => {
-    console.log("You swiped: " + direction);
     if (direction === "right") {
       setLikes((prevLikes) => prevLikes + 1);
     } else if (direction === "left") {
       setDislikes((prevDislikes) => prevDislikes + 1);
     }
-    setImageIndex((prevIndex) => prevIndex + 1);
   };
 
   const onCardLeftScreen = (myIdentifier: string) => {
     console.log(myIdentifier + " left the screen");
   };
 
-  const handleNextImage = () => {
-    setImageIndex((prevIndex) => prevIndex + 1);
-  };
-
   const handlePrevImage = () => {
     setImageIndex((prevIndex) => prevIndex - 1);
   };
 
-  if (!TinderCard) return null; // render nothing if TinderCard is not loaded yet
+  if (!TinderCardLoaded) return null; // render nothing if TinderCard is not loaded yet
 
   return (
     <div className="text-white">
