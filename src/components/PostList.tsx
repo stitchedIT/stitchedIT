f3b740b38019455059bc176747227d92065b5a7e
“Xhesika” <xhesikam2184@tywls.org>
2023-08-25 19:34:09 -0400

    added comment feature

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,5 +1,6 @@
 import { Post } from "~/types";
 import { api } from "~/utils/api";
+import { useState } from "react";
 import {Button} from "@/components/ui/button";
 import { getAuth, buildClerkProps } from "@clerk/nextjs/server";
 
@@ -12,16 +13,26 @@ function PostList({ userId }: Props) {
   const posts = api.post.getAllPosts.useQuery();
   let postsData = posts.data
   const createLike = api.post.toggleLike.useMutation();
-
+  const createComment = api.post.addComment.useMutation(); 
   console.log(postsData)
 
+  const [comment, setComment] = useState('');
+  const [likes, setLikes] = useState(0);
   const handleLike = (userId: string, id: number) => {
     console.log('liked');
     createLike.mutate({
       userId: userId,
       postId: id,
     })
-    console.log("hi")
+    // setLikes();
+  }
+  const handleComment = (userId: string, id: number) => {
+    createComment.mutate({
+      userId: userId,
+      postId: id,
+      content: comment,
+    })
+    setComment('');
   }
 
   return (
@@ -34,7 +45,13 @@ function PostList({ userId }: Props) {
           <p>{likesCount}</p>
           <Button className="bg-stitched-lightPink" variant="outline">Save</Button>
           <Button variant="outline" className="bg-stitched-lightPink" onClick={() => handleLike(userId, id)}>Like</Button>
-          <Button className="bg-stitched-lightPink" variant="outline">Comment</Button>
+          <input
+            type="text"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            placeholder="Write a comment..."
+          />
+          <Button className="bg-stitched-lightPink" variant="outline" onClick={() => handleComment(userId, id)}>Comment</Button>
         </div>
       ))}
     </div>
