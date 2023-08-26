78b796c5aa6d0b595d9d0532ca9ce545d4083f7c
“Xhesika” <xhesikam2184@tywls.org>
2023-08-26 13:14:14 -0400

    finished nav bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,12 +5,22 @@ import {
   useUser,
 } from "@clerk/nextjs";
 import { buttonVariants } from "@/components/ui/button";
+import {Button} from "@/components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Router from "next/router";
 
 function Navbar() {
   const staticStyles =
     "inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-[#2e026d] hover:bg-white hover:cursor-pointer";
   const user = useUser();
+  const router = Router;
+
+  const handleFeedButton = () => {
+    router.push("/feed");
+  }
+  const handleBookmarkButton = () => {
+    router.push("/bookmarks");
+  }
 
   return (
     <nav className="sticky top-0 flex items-center justify-between bg-transparent p-6">
@@ -29,14 +39,22 @@ function Navbar() {
       </div>
       <div className="flex gap-4">
         {user.isSignedIn ? (
-          <SignOutButton>
-            <span
-              className={`${staticStyles} ${buttonVariants({
-                variant: "outline",
-              })}`}>
-              Logout
-            </span>
-          </SignOutButton>
+          <>
+            <Button className={`${staticStyles} ${buttonVariants({
+                  variant: "outline",
+                })}`} onClick={() => handleFeedButton()}> Feed</Button>
+            <Button className={`${staticStyles} ${buttonVariants({
+                  variant: "outline",
+                })}`} onClick={() => handleBookmarkButton()}> Bookmarks</Button>
+            <SignOutButton>
+              <span
+                className={`${staticStyles} ${buttonVariants({
+                  variant: "outline",
+                })}`}>
+                Logout
+              </span>
+            </SignOutButton>
+          </>
         ) : (
           <>
             <SignInButton>
