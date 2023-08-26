f6d5d24976f90607c30bad57fbb9bf545996a6b8
JasonPaulino <jasonp0830@gmail.com>
2023-08-26 15:21:29 -0400

    skeleton for landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
-// NextJS utils
 import Head from "next/head";
-import Image from "next/image";
 import { type NextPage } from "next";
+import Image from "next/image";
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
 
 const HomePage: NextPage = () => {
-  
+  const staticStyles =
+    "inline-flex items-center justify-center text-sm px-4 py-2 leading-none border rounded text-white border-stitched-pink hover:border-transparent hover:text-stitched-pink hover:bg-white hover:cursor-pointer";
   return (
     <>
       <Head>
@@ -12,22 +14,38 @@ const HomePage: NextPage = () => {
         <meta name="description" content="An app to explore new clothes." />
         <link rel="icon" href="/00.png" />
       </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            <span className="text-[hsl(280,100%,70%)]">stitchedIT</span>
-          </h1>
-
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8"></div>
-          <div className="flex flex-col items-center gap-2">
-            <Image width={250} height={250} src={`/00.png`} alt="stitchedIT" />
+      <main className="flex min-h-screen items-center justify-center bg-stitched-black px-11">
+        <div className="flex flex-col items-start">
+          <div style={{ display: "inline-block" }}>
+            <h1 className="text-5xl font-bold text-stitched-pink">
+              Discover a world of fashion inspiration with our curated clothing.
+            </h1>
+            <div className="p-3" />
+            <p className="text-xl text-white">
+              Unleash your inner fashionista and embark on a journey through our
+              meticulously curated clothing collection, where every piece is a
+              canvas for your unique style expression.
+            </p>
+            <div className="p-3" />
+            <Link
+              href="/home"
+              className={`${staticStyles} ${buttonVariants({
+                variant: "outline",
+              })}`}
+            >
+              Get Started
+            </Link>
           </div>
         </div>
+        <Image
+          src="/00.png"
+          alt="landing-page-image"
+          width={350}
+          height={350}
+        />
       </main>
     </>
   );
-}
-
-
+};
 
 export default HomePage;
