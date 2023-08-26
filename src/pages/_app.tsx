b7d14eb6f929fb47779d683013242b274260ce8c
“Xhesika” <xhesikam2184@tywls.org>
2023-08-26 13:46:09 -0400

    updated nav with colors and bookmark and feed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,12 @@ import { ClerkProvider } from "@clerk/nextjs";
 import type { AppProps } from "next/app";
 import { api } from "~/utils/api";
 import '@/globals.css'
+import Navbar from "~/components/NavBar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ClerkProvider {...pageProps}>
+      <Navbar />
       <Component {...pageProps} />
     </ClerkProvider>
   );
