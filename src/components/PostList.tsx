dbfcc9f15b6c89cb6b47e1e7264fc970bbd352a5
“Xhesika” <xhesikam2184@tywls.org>
2023-08-28 20:41:58 -0400

    delete comments

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -8,13 +8,17 @@ type PostProps = {
   post: PostType;
   userId: string;
 };
-
 const Post: React.FC<PostProps> = ({ post, userId }) => {
+  const [showComments, setShowComments] = useState(false); // Add state variable for showing comments
   const createLike = api.post.toggleLike.useMutation();
   const createComment = api.post.addComment.useMutation(); 
   const deletePost = api.post.deletePost.useMutation();
+  const deleteComment = api.post.deleteComment.useMutation();
   const savePost = api.post.bookmarkPost.useMutation();
   const [comment, setComment] = useState('');
+  const commentsQuery = api.post.getCommentsByPostId.useQuery({ postId: post.id });
+  const [clickCount, setClickCount] = useState(2);
+  const [comments, setComments] = useState<any[]>(commentsQuery.data ? [...commentsQuery.data] : []);
 
   const handleLike = (id: number) => {
     createLike.mutate({
@@ -27,21 +31,48 @@ const Post: React.FC<PostProps> = ({ post, userId }) => {
      postId: id
    })
  }
+ const handleDeleteComment = (id: number) => {
+   deleteComment.mutate({
+      postId: post.id,
+     commentId: id
+   })
+ }
  const handleSave = (id: number) => {
    savePost.mutate({
      userId: userId,
      postId: id
    })
  }
+//  const handleViewComments = (id: number) => {
+//   setClickCount(clickCount + 1);
+//   if(clickCount % 2 === 0){ // Show comments when view comments is clicked
+//     setShowComments(true);
+//   } else {
+//     setShowComments(false);
+//   };
+// };
+const handleViewComments = (id: number) => {
+  setClickCount(clickCount + 1);
+  setShowComments(clickCount % 2 === 0);
+};
+const handleComment = (id: number) => {
+  createComment.mutate({
+    userId: userId,
+    postId: id,
+    content: comment,
+  });
+  setComment('');
+  setComments([...comments, { id: comments.length + 1, content: comment }]);
+};
+  // const handleComment = (id: number) => {
+  //   createComment.mutate({
+  //     userId: userId,
+  //     postId: id,
+  //     content: comment,
+  //   })
+  //   setComment('');
 
-  const handleComment = (id: number) => {
-    createComment.mutate({
-      userId: userId,
-      postId: id,
-      content: comment,
-    })
-    setComment('');
-  }
+  // }
 const isOwner = userId === post.userId;
   return (
     <div key={post.id}>
@@ -59,13 +90,20 @@ const isOwner = userId === post.userId;
         placeholder="Write a comment..."
       />
       <Button className="bg-stitched-lightPink" variant="outline" onClick={() => handleComment(post.id)}>Comment</Button>
+      <Button className="bg-stitched-lightPink" variant="outline" onClick={() => handleViewComments(post.id)}>View Comments</Button>
+      {showComments && (
+        <>
+          {commentsQuery.data?.map((comment) => (
+            <div key={comment.id}>
+              <p>{comment.content}</p>
+              <Button className="bg-stitched-lightPink" variant="outline" onClick={() => handleDeleteComment(comment.id)}>Delete</Button>
+            </div>
+      ))}
+        </>
+      )}
     </div>
   );
 }
-
-type PostListProps = {
-  userId: string
-};
 
 function PostList({ userId }: PostListProps) {
   const posts = api.post.getAllPosts.useQuery();
