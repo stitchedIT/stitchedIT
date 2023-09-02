e440b3cdac719ea7cdb1217be4e913975afe5f15
JasonPaulino <jasonp0830@gmail.com>
2023-09-02 13:49:51 -0400

    older home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,55 +1,15 @@
 import Head from "next/head";
 import { type NextPage } from "next";
 import Image from "next/image";
+import { buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
-type SwitchProps = {
-  userId: string;
-}
-export const getServerSideProps: GetServerSideProps<SwitchProps> = async (ctx:GetServerSidePropsContext) => {
-  // const { userId } = getAuth(ctx.req);
-  const { userId } = getAuth(ctx.req);
-  if (!userId) {
-    return {
-      redirect: {
-        destination: "/login",
-        permanent: false, 
-      },
-    };
-  }
-  
-  return { props: { ...buildClerkProps(ctx.req), userId } };
-};
-
-const HomePage: NextPage = (userId: any) => {
-  
 
 // Components
 import Navbar from "~/components/NavBar";
 import SwipeableComponent from "~/components/SwipeableComponent";
 import Footer from "~/components/Footer";
-import Image from "next/image";
-import Link from "next/link";
-type SwitchProps = {
-  userId: string;
-};
-export const getServerSideProps: GetServerSideProps<SwitchProps> = async (
-  ctx: GetServerSidePropsContext
-) => {
-  // const { userId } = getAuth(ctx.req);
-  const { userId } = getAuth(ctx.req);
-  if (!userId) {
-    return {
-      redirect: {
-        destination: "/login",
-        permanent: false,
-      },
-    };
-  }
 
-  return { props: { ...buildClerkProps(ctx.req), userId } };
-};
-
-const HomePage: NextPage = (userId: any) => {
+const HomePage: NextPage = () => {
   return (
     <>
       <Head>
@@ -63,18 +23,13 @@ const HomePage: NextPage = (userId: any) => {
           <h1 className="text-4xl font-bold text-stitched-pink sm:text-5xl md:text-4xl lg:text-6xl">
             Discover a world of fashion inspiration.
           </h1>
-
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8"></div>
-          <div className="flex flex-col items-center gap-2">
-            <Image width={250} height={250} src={`/00.png`} alt="stitchedIT" />
-            <SwipeableComponent userId={userId.userId} />
-          </div>
-          <p className="text-lg text-white md:text-2xl">
+          {/* Adjusted text size for smaller screens */}
+          <p className="text-lg text-white sm:text-xl md:text-lg lg:text-2xl">
             Unleash your inner fashionista. Every piece in our collection is a
             canvas for your unique style.
           </p>
           <Link
-            className="inline-flex cursor-pointer items-center justify-center rounded border border-stitched-pink px-6 py-3 text-lg leading-none text-white transition duration-300 ease-in-out hover:border-transparent hover:bg-white hover:text-stitched-pink md:text-xl"
+            className="inline-flex cursor-pointer items-center justify-center rounded border border-stitched-pink px-4 py-2 text-base leading-none text-white transition duration-300 ease-in-out hover:border-transparent hover:bg-white hover:text-stitched-pink md:px-6 md:py-3 md:text-lg lg:text-xl"
             href="/home"
           >
             Get Started
@@ -84,7 +39,7 @@ const HomePage: NextPage = (userId: any) => {
               viewBox="0 0 15 15"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
-              className="ml-3"
+              className="ml-2 md:ml-3"
             >
               <path
                 d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z"
