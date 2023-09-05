44184c11d5c321491dd3e561d120de49dcb8a76f
JasonPaulino <jasonp0830@gmail.com>
2023-09-05 02:25:42 -0400

    implemented a simpler toggling system for the showComments, removed unused imports, reset the comments after making comment, only post user can delete their own comments

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -2,24 +2,19 @@ import { Post as PostType } from "~/types";
 import { api } from "~/utils/api";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
-import { get } from "http";
-import { Toaster } from "@/components/ui/toaster"
-import { useToast } from "@/components/ui/use-toast"
-
-
-
+import { Toaster } from "@/components/ui/toaster";
+import { useToast } from "@/components/ui/use-toast";
 
 type PostProps = {
   post: PostType;
   userId: string;
 };
+
 const Post: React.FC<PostProps> = ({ post, userId }) => {
-  const [showComments, setShowComments] = useState(false); // Add state variable for showing comments
+  const [showComments, setShowComments] = useState(false);
   const [likes, setLikes] = useState(post.likesCount);
   const createLike = api.post.toggleLike.useMutation();
-  const getLikes = api.post.getLikesByPostId.useQuery({
-    postId: post.id,
-  });
+  const getLikes = api.post.getLikesByPostId.useQuery({ postId: post.id });
   const createComment = api.post.addComment.useMutation();
   const deletePost = api.post.deletePost.useMutation();
   const deleteComment = api.post.deleteComment.useMutation();
@@ -28,18 +23,14 @@ const Post: React.FC<PostProps> = ({ post, userId }) => {
   const commentsQuery = api.post.getCommentsByPostId.useQuery({
     postId: post.id,
   });
-  const [clickCount, setClickCount] = useState(2);
   const [comments, setComments] = useState<any[]>(
     commentsQuery.data ? [...commentsQuery.data] : []
   );
-  const { toast } = useToast()
+  const { toast } = useToast();
 
   const handleLike = (id: number) => {
     createLike.mutate(
-      {
-        userId: userId,
-        postId: id,
-      },
+      { userId: userId, postId: id },
       {
         onSuccess: async () => {
           setLikes((await getLikes.refetch()).data.length);
@@ -49,71 +40,39 @@ const Post: React.FC<PostProps> = ({ post, userId }) => {
   };
 
   const handleDelete = (id: number) => {
-    deletePost.mutate(
-      {
-        postId: id,
-      },
+    deletePost.mutate({ postId: id });
+  };
+
+  const handleDeleteComment = (id: number) => {
+    deleteComment.mutate({ postId: post.id, commentId: id });
+  };
+
+  const handleSave = (id: number) => {
+    savePost.mutate({ userId: userId, postId: id });
+    toast({ title: "Post Saved", description: "You have great taste!" });
+  };
+
+  const handleViewComments = () => {
+    setShowComments(!showComments);
+  };
+
+  const handleComment = (id: number) => {
+    createComment.mutate(
+      { userId: userId, postId: id, content: comment },
       {
         onSuccess: async () => {
-          
+          setComments(await commentsQuery.refetch().data);
         },
       }
     );
-  };
-  const handleDeleteComment = (id: number) => {
-    deleteComment.mutate({
-      postId: post.id,
-      commentId: id,
-    });
-  };
-  const handleSave = (id: number) => {
-    savePost.mutate({
-      userId: userId,
-      postId: id,
-    });
-    toast({
-      title: "Post Saved",
-      description: "You have great taste!",
-    })
-  };
-
-  const handleViewComments = (id: number) => {
-    setClickCount(clickCount + 1);
-    setShowComments(clickCount % 2 === 0);
-  };
-  // {
-  //   createLike.mutate(
-  //     {
-  //       userId: userId,
-  //       postId: id,
-  //     },
-  //     {
-  //       onSuccess: async () => {
-  //         setLikes((await getLikes.refetch()).data.length);
-  //       },
-  //     }
-  //   );
-  // };
-  const handleComment = (id: number) => {
-    createComment.mutate(
-      {
-      userId: userId,
-      postId: id,
-      content: comment,
-    }, 
-    {
-      onSuccess: async () => {
-      setComments((await commentsQuery.refetch().data))
-            }, 
-    });
-    // setComment("");
-    // setComments([...comments, { id: comments.length + 1, content: comment }]);
+    setComment("");
   };
 
   const isOwner = userId === post.userId;
+
   return (
     <div
-      className="mx-auto mt-6 flex w-full max-w-3xl flex-col items-center justify-center rounded-lg  bg-stitched-black p-8 text-white shadow-md outline-dashed"
+      className="mx-auto mt-6 flex w-full max-w-3xl flex-col items-center justify-center rounded-lg bg-stitched-black p-8 text-white shadow-md outline-dashed"
       key={post.id}
     >
       <img src={post.imageUrl} width={600} alt="post" />
@@ -129,16 +88,16 @@ const Post: React.FC<PostProps> = ({ post, userId }) => {
           Save
         </Button>
         <Button
-          variant="outline"
           className="ml-2 bg-stitched-lightPink"
+          variant="outline"
           onClick={() => handleLike(post.id)}
         >
           Like
         </Button>
         {isOwner && (
           <Button
-            variant="outline"
             className="ml-2 bg-stitched-lightPink"
+            variant="outline"
             onClick={() => handleDelete(post.id)}
           >
             Delete
@@ -160,9 +119,9 @@ const Post: React.FC<PostProps> = ({ post, userId }) => {
         Comment
       </Button>
       <Button
-        className="bg-stitched-lightPink "
+        className="bg-stitched-lightPink"
         variant="outline"
-        onClick={() => handleViewComments(post.id)}
+        onClick={handleViewComments}
       >
         View Comments
       </Button>
@@ -171,13 +130,15 @@ const Post: React.FC<PostProps> = ({ post, userId }) => {
           {commentsQuery.data?.map((comment) => (
             <div key={comment.id}>
               <p>{comment.content}</p>
-              <Button
-                className="bg-stitched-lightPink"
-                variant="outline"
-                onClick={() => handleDeleteComment(comment.id)}
-              >
-                Delete
-              </Button>
+              {isOwner && (
+                <Button
+                  className="bg-stitched-lightPink"
+                  variant="outline"
+                  onClick={() => handleDeleteComment(comment.id)}
+                >
+                  Delete
+                </Button>
+              )}
             </div>
           ))}
         </>
@@ -186,19 +147,20 @@ const Post: React.FC<PostProps> = ({ post, userId }) => {
   );
 };
 
+type PostListProps = {
+  userId: string;
+};
+
 function PostList({ userId }: PostListProps) {
   const posts = api.post.getAllPosts.useQuery();
   let postsData = posts.data;
-  
-  
 
   return (
     <div>
       {postsData?.map((post) => (
         <Post key={post.id} post={post} userId={userId} />
       ))}
-    <Toaster />
-
+      <Toaster />
     </div>
   );
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,35 +5,37 @@ import { getAuth, buildClerkProps } from "@clerk/nextjs/server";
 import { GetServerSidePropsContext } from "next";
 import SwipeableComponent from "~/components/SwipeableComponent";
 type SwitchProps = {
-    userId: string;
+  userId: string;
+};
+
+export const getServerSideProps: GetServerSideProps<SwitchProps> = async (
+  ctx: GetServerSidePropsContext
+) => {
+  // const { userId } = getAuth(ctx.req);
+  const { userId } = getAuth(ctx.req);
+  if (!userId) {
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
   }
 
-export const getServerSideProps: GetServerSideProps<SwitchProps> = async (ctx:GetServerSidePropsContext) => {
-    // const { userId } = getAuth(ctx.req);
-    const { userId } = getAuth(ctx.req);
-    if (!userId) {
-      return {
-        redirect: {
-          destination: "/login",
-          permanent: false, 
-        },
-      };
-    }
-    
-    return { props: { ...buildClerkProps(ctx.req), userId } };
-  };
+  return { props: { ...buildClerkProps(ctx.req), userId } };
+};
 const HomePage: NextPage = (userId: any) => {
-    return (
-        <>
-            <Head>
-                <title>Home Page</title>
-                <meta name="description" content="An app to explore new clothes." />
-                <link rel="icon" href="/00.png" />
-            </Head>
-            <h1>This is the Home page</h1>
-            <SwipeableComponent userId={userId.userId}/>
-        </>
-    )
-}
+  return (
+    <>
+      <Head>
+        <title>Home Page</title>
+        <meta name="description" content="An app to explore new clothes." />
+        <link rel="icon" href="/00.png" />
+      </Head>
+      <h1>This is the Home page</h1>
+      <SwipeableComponent userId={userId.userId} />
+    </>
+  );
+};
 
 export default HomePage;
