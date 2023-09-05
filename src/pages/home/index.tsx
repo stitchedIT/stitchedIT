6d8f819c27780ab7e0a54b14cbdaa8c73c0db4a9
“Xhesika” <xhesikam2184@tywls.org>
2023-09-05 15:00:21 -0400

    acepted change

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -24,7 +24,6 @@ export const getServerSideProps: GetServerSideProps<SwitchProps> = async (
     };
   }
 
-<<<<<<< HEAD
   return { props: { ...buildClerkProps(ctx.req), userId } };
 };
 const HomePage: NextPage = (userId: any) => {
@@ -40,47 +39,5 @@ const HomePage: NextPage = (userId: any) => {
     </>
   );
 };
-=======
-
-export const getServerSideProps: GetServerSideProps<SwitchProps> = async (ctx:GetServerSidePropsContext) => {
-    
-    const { userId } = getAuth(ctx.req);
-    
-    if (!userId) {
-      return {
-        redirect: {
-          destination: "/login",
-          permanent: false, 
-        },
-      };
-    }
-    
-    return { props: { ...buildClerkProps(ctx.req), userId } };
-  };
-const HomePage: NextPage = (userId: any) => {
-    const { user } = useUser();
-    console.log(user)
-
-    // async function  getUser(){
-    //     let dog = await api.user.getUserById.useQuery({
-    //         id: userId.userId,
-    //     })
-    //     console.log(dog, "dog")
-    // }
-    // getUser()
-   
-    return (
-        <>
-            <Head>
-                <title>Home Page</title>
-                <meta name="description" content="An app to explore new clothes." />
-                <link rel="icon" href="/00.png" />
-            </Head>
-            
-            <SwipeableComponent userId={userId.userId}/>
-        </>
-    )
-}
->>>>>>> main
 
 export default HomePage;
