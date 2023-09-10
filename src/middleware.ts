ad60d906205e3cb082fa294ce6d4fd7cbbb2b7bc
“Xhesika” <xhesikam2184@tywls.org>
2023-09-09 23:38:26 -0400

    fixed singout bug

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,12 @@
 import { authMiddleware } from "@clerk/nextjs";
 
+const afterSignOutCallback = ({ clerk }) => {
+  // Redirect to your landing page after signout
+  clerk.signOut().then(() => {
+    window.location.href = "/";
+  });
+};
+
 export default authMiddleware({
   publicRoutes: ["/", "/api/clerk/createUser"],
 });
